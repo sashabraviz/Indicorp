669420eef9e18a7db9f5db6d7358b59b7acac277
Sasha.doayarchyc <sashadoadr@gmail.com>
2022-08-10 15:48:34 +0300

    Indicorp

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import * as flsFunctions from "./modules/functions.js";
+import * as Burger from "./modules/header-burger.js";
+import * as Header_show from "./modules/header_scroll.js";
+
+flsFunctions.isWebp();
+
+Burger.isBurger();
+
+Header_show.isHeader_show();
